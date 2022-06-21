0b72c6e04a7b7c1a4816b8d6dc4c77c2f646ad0a
Rick Huijzer <r.huijzer@dca-ict.nl>
2022-06-21 13:43:05 +0200

    st -> alacritty

--- config.def.h
+++ config.def.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-theme", "Pop-Dark", "-font", "\"Fira Code 15\"", "-show", "drun", NULL};
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 #include <X11/XF86keysym.h>
 
