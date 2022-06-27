708b3a31875cd5eea718fd3ce62652821dbb57ef
Rick Huijzer <r.huijzer@dca-ict.nl>
2022-06-27 11:14:56 +0200

    xscreensaver lock

--- config.def.h
+++ config.def.h
@@ -107,6 +107,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+  { MODKEY|ShiftMask,		XK_l,		spawn,		SHCMD("xscreensaver-command -lock") },
 };
 
 /* button definitions */
